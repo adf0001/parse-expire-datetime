fa70185132dc4c364ae1ae654a38d418f726b405
fwg <adf0001@163.com>
2022-09-28 21:32:35 +0800

    build git

---
+++ parse-expire-datetime.js
@@ -0,0 +1,133 @@
+﻿
+//Convert a local-fake-timezone Date to a real local Date.
+function timezoneToLocal(dt, timezoneOffset) {
+	return new Date(dt.getTime() + (timezoneOffset - dt.getTimezoneOffset()) * 60000);
+}
+
+//Convert a real local Date to a local-fake-timezone Date.
+function localToTimezone(dt, timezoneOffset) {
+	return new Date(dt.getTime() - (timezoneOffset - dt.getTimezoneOffset()) * 60000);
+}
+
+//MM: 1-12, dd: 1-31
+function createExpireDate(YYYY, MM, dd, hh, mm, ss, sss, timezoneOffset) {
+	YYYY = parseInt(YYYY, 10);
+	MM = parseInt(MM, 10) - 1;
+	dd = parseInt(dd, 10) - 1;
+	hh = parseInt(hh, 10);
+	mm = parseInt(mm, 10);
+	ss = parseInt(ss, 10);
+	sss = parseInt(sss, 10);
+
+	var dt = new Date(YYYY || 0, MM || 0, (dd || 0) + 1, hh || 0, mm || 0, ss || 0, sss || 0);
+
+	if (isNaN(hh)) dt.setHours(23);
+	if (isNaN(mm)) dt.setMinutes(59);
+	if (isNaN(ss)) dt.setSeconds(59);
+	if (isNaN(sss)) dt.setMilliseconds(999);
+
+	//timezoneOffset
+	return (typeof timezoneOffset === "number") ? timezoneToLocal(dt, timezoneOffset) : dt;
+}
+
+//utcTime: refer to Date.prototype.getTime()
+function offsetExpireDate(utcTime, dd, hh, mm, ss, sss, timezoneOffset) {
+	if (typeof utcTime !== "number") utcTime = (new Date()).getTime();
+
+	var dt = new Date(utcTime);
+	if (typeof timezoneOffset === "number") dt = localToTimezone(dt, timezoneOffset);
+
+	return createExpireDate(
+		dt.getFullYear(),
+		dt.getMonth() + 1,
+		dt.getDate() + (parseInt(dd, 10) || 0),
+		dt.getHours() + parseInt(hh, 10),
+		dt.getMinutes() + parseInt(mm, 10),
+		dt.getSeconds() + parseInt(ss, 10),
+		dt.getMilliseconds() + parseInt(sss, 10),
+		timezoneOffset
+	);
+}
+
+/*
+Parse string to expire datetime.
+
+parseExpireDatetime(str, options | timezoneOffset | now )
+	options
+		timezoneOffset
+			Number type, in minutes, refer to Date.prototype.getTimezoneOffset().
+
+		now
+			A Date object, for debug purpose, only available for offset type string.
+
+Return a date object, or empty if fail.
+*/
+function parseExpireDatetime(str, options) {
+	//arguments
+
+	//str
+	if (!str) return;
+	str = str.replace(/(^\s+|\s+$)/g, "");	//remove head/tail spaces
+	if (!str) return;
+
+	//options
+	if (typeof options === "number") options = { timezoneOffset: options };
+	else if (options && (options instanceof Date)) options = { now: options };
+
+	//parsing start
+
+	var mr;		//match result
+
+	// YYYY-MM-DD( hh(:mm(:ss(.sss)?)?)?)?, as YYYY-MM-DD (hh|23):(mm|59):(ss|59).(sss|999)
+	if (mr = str.match(/^(\d+)\-(\d+)\-(\d+)(\s+\d+(\:\d+(\:\d+(\.\d+)?)?)?)?$/)) {
+		return createExpireDate(
+			mr[1], mr[2], mr[3],		//YYYY-MM-DD
+			mr[4], mr[5]?.slice(1), mr[6]?.slice(1), mr[7]?.slice(1),	//hh:mm:ss.sss
+			options?.timezoneOffset
+		);
+	}
+	// YYYY-MM, as YYYY-MM-(last-day-of-month) 23:59:59.999	
+	else if (mr = str.match(/^(\d+)\-(\d+)$/)) {
+		return createExpireDate(
+			mr[1],	//YYYY
+			(parseInt(mr[2], 10) || 1) + 1,	//MM+1
+			1,		//1st
+			0, 0, 0, -1,		//00:00:00.000 - 1 = 23:59:59.999
+			options?.timezoneOffset);
+	}
+	// dd( hh(:mm(:ss(.sss)?)?)?)?, as now + ( dd (hh|00):(mm|00):(ss|00).(sss|000) ), then set hh/mm/ss/sss to 23/59/59/999 if they are omitted.
+	else if (mr = str.match(/^(\d+)(\s+\d+(\:\d+(\:\d+(\.\d+)?)?)?)?$/)) {
+		return offsetExpireDate(options?.now?.getTime(),
+			mr[1], mr[2], mr[3]?.slice(1), mr[4]?.slice(1), mr[5]?.slice(1),
+			options?.timezoneOffset);
+	}
+	// hh:(mm(:ss(.sss)?)?)?, as now + ( hh:(mm|00):(ss|00).(sss|000) ), then set mm/ss/sss to 59/59/999 if they are omitted.
+	else if (mr = str.match(/^(\d+)\:(\d+(\:\d+(\.\d+)?)?)?$/)) {
+		return offsetExpireDate(options?.now?.getTime(),
+			0, mr[1], mr[2], mr[3]?.slice(1), mr[4]?.slice(1),
+			options?.timezoneOffset);
+	}
+	// ddD? hhH? mmM? ss.sssS?, as now + ( dd (hh|00):(mm|00):(ss|00).(sss|000) ).
+	else if (mr = str.match(/^(\d+d\s*)?(\d+h\s*)?(\d+m\s*)?(?:(\d*)(\.\d+)?s)?$/i)) {
+		return offsetExpireDate(options?.now?.getTime(),
+			mr[1] || 0, mr[2] || 0, mr[3] || 0, mr[4] || 0, mr[5]?.slice(1) || 0,
+			options?.timezoneOffset);
+	}
+	/*
+	*/
+}
+
+// module exports
+module.exports = {
+	timezoneToLocal,
+	localToTimezone,
+
+	createExpireDate,
+	create: createExpireDate,
+
+	offsetExpireDate,
+	offset: offsetExpireDate,
+
+	parseExpireDatetime,
+	parse: parseExpireDatetime,
+};
---
+++ test/test-data.js
@@ -0,0 +1,129 @@
+
+//global variable, for html page, refer tpsvr @ npm.
+parse_expire_datetime = require("../parse-expire-datetime.js");
+var { parseExpireDatetime } = parse_expire_datetime;
+
+//Date to "YYYY-MM-DD hh:mm:ss"
+var toString19 = function (dt, toUtc) {
+	if (!dt) dt = new Date();
+
+	var s = toUtc ?
+		(dt.getUTCFullYear() + "-" + (dt.getUTCMonth() + 1) + "-" + dt.getUTCDate() + " " + dt.getUTCHours() + ":" + dt.getUTCMinutes() + ":" + dt.getUTCSeconds())
+		:
+		(dt.getFullYear() + "-" + (dt.getMonth() + 1) + "-" + dt.getDate() + " " + dt.getHours() + ":" + dt.getMinutes() + ":" + dt.getSeconds());
+
+	return s.replace(/\b(\d)\b/g, "0$1");
+}
+
+//Date to "YYYY-MM-DD hh:mm:ss.fff"
+var toString23 = function (dt, toUtc) {
+	if (!dt) dt = new Date();
+	return toString19(dt) + "." + ("00" + (toUtc ? dt.getUTCMilliseconds() : dt.getMilliseconds())).slice(-3);
+}
+
+cmp = function (str, localExpect, options) {
+	var dt = parseExpireDatetime(str, options);
+	if (!dt) {
+		console.error("fail to parse, " + str);
+		return false;	//format fail
+	}
+
+	var str2 = toString23(dt);
+
+	if (str2 === localExpect) return true;
+
+	console.error("input string: " + str);
+	console.error("value string: " + str2);
+	console.error("the expected: " + localExpect);
+}
+
+module.exports = {
+
+	"parse_expire_datetime": function (done) {
+		//if (typeof window !==/=== "undefined") throw "disable for browser/nodejs";
+
+		var dtNow = new Date(2022, 0, 1, 10, 20, 30, 40);	//for debug, 2022-01-01 10:20:30.040
+
+		done(!(
+			// YYYY-MM-DD( hh(:mm(:ss(.sss)?)?)?)?, as YYYY-MM-DD (hh|23):(mm|59):(ss|59).(sss|999)
+			cmp("2000-1-2 12:34:56.78", "2000-01-02 12:34:56.078") &&
+			cmp("2000-1-2 12:34:56", "2000-01-02 12:34:56.999") &&
+			cmp("2000-1-2 12:34", "2000-01-02 12:34:59.999") &&
+			cmp("2000-1-2 12", "2000-01-02 12:59:59.999") &&
+			cmp("2000-1-2", "2000-01-02 23:59:59.999") &&
+
+			cmp("2000-1-2", "2000-01-02 22:59:59.999",
+				{ timezoneOffset: (new Date()).getTimezoneOffset() - 60 }) &&		//timezoneOffset
+
+			parseExpireDatetime("2000-01-01 00:00:00.000", 0).getTime() ===		//UTC+0
+			Date.UTC(2000, 0, 1, 0, 0, 0, 0) &&
+
+			parseExpireDatetime("2000-01-01 00:00:00.000", 0).getTime() ===		//UTC+0
+			parseExpireDatetime("2000-01-01 08:00:00.000", -480).getTime() &&	//UTC+8
+
+			// YYYY-MM, as YYYY-MM-(last-day-of-month) 23:59:59.999	
+			cmp("2000-1", "2000-01-31 23:59:59.999") &&
+			cmp("2000-2", "2000-02-29 23:59:59.999") &&
+			cmp("2001-2", "2001-02-28 23:59:59.999") &&
+
+			// DDD( hh(:mm(:ss(.sss)?)?)?)?, as now + ( DDD (hh|00):(mm|00):(ss|00).(sss|000) ), then set hh/mm/ss/sss to 23/59/59/999 if they are omitted.
+			cmp("1 3:4:5.6", "2022-01-02 13:24:35.046", { now: dtNow }) &&
+			cmp("1 3:4:5", "2022-01-02 13:24:35.999", dtNow) &&
+			cmp("1 3:4", "2022-01-02 13:24:59.999", dtNow) &&
+			cmp("1 3", "2022-01-02 13:59:59.999", dtNow) &&
+			cmp("1", "2022-01-02 23:59:59.999", dtNow) &&
+
+			// hh:(mm(:ss(.sss)?)?)?, as now + ( hh:(mm|00):(ss|00).(sss|000) ), then set mm/ss/sss to 59/59/999 if they are omitted.
+			cmp("3:4:5.6", "2022-01-01 13:24:35.046", dtNow) &&
+			cmp("3:4:5", "2022-01-01 13:24:35.999", dtNow) &&
+			cmp("3:4", "2022-01-01 13:24:59.999", dtNow) &&
+			cmp("3:", "2022-01-01 13:59:59.999", dtNow) &&
+
+			cmp("3:124", "2022-01-01 15:24:59.999", dtNow) &&	//value out of range
+
+			// ddD? hhH? mmM? ss.sssS?, as now + ( dd (hh|00):(mm|00):(ss|00).(sss|000) ).
+			cmp("1d3h4m5.6s", "2022-01-02 13:24:35.046", dtNow) &&
+			cmp("1d3h4m5s", "2022-01-02 13:24:35.040", dtNow) &&
+			cmp("1d3h4m", "2022-01-02 13:24:30.040", dtNow) &&
+			cmp("1d3h", "2022-01-02 13:20:30.040", dtNow) &&
+			cmp("1d", "2022-01-02 10:20:30.040", dtNow) &&
+
+			cmp("3h", "2022-01-01 13:20:30.040", dtNow) &&
+			cmp("4m", "2022-01-01 10:24:30.040", dtNow) &&
+			cmp("5s", "2022-01-01 10:20:35.040", dtNow) &&
+			cmp("0.6s", "2022-01-01 10:20:30.046", dtNow) &&
+			cmp(".6s", "2022-01-01 10:20:30.046", dtNow) &&
+
+			cmp(" 1D   3H4m    5.6s    ", "2022-01-02 13:24:35.046", dtNow) &&	//spaces & cases
+
+			cmp(" 1D   4m ", "2022-01-02 10:24:30.040", dtNow) &&	//partial
+			cmp(" 3H    5s    ", "2022-01-01 13:20:35.040", dtNow) &&
+			cmp(" 3H    .6s    ", "2022-01-01 13:20:30.046", dtNow) &&
+			/*
+			*/
+
+			true
+		));
+	},
+
+	"check exports": function (done) {
+		var m = parse_expire_datetime;
+		for (var i in m) {
+			if (typeof m[i] === "undefined") { done("undefined: " + i); return; }
+		}
+		done(false);
+
+		console.log(m);
+		var list = "export list: " + Object.keys(m).join(", ");
+		console.log(list);
+		return list;
+	},
+
+};
+
+// for html page
+//if (typeof setHtmlPage === "function") setHtmlPage("title", "10em", 1);	//page setting
+if (typeof showResult !== "function") showResult = function (text) { console.log(text); }
+
+//for mocha
+if (typeof describe === "function") describe('parse_expire_datetime', function () { for (var i in module.exports) { it(i, module.exports[i]).timeout(5000); } });
